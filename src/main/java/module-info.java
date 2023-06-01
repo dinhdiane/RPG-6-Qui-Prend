d9045c4936ca199efe5228d9a8f83cd421809e41
holymoonight <diane.dinh@eleve.isep.fr>
2023-06-01 16:09:52 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module sixquiprend.javafx.sixquiprenddiane {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens sixquiprend.javafx.sixquiprenddiane to javafx.fxml;
+    exports sixquiprend.javafx.sixquiprenddiane;
+}
